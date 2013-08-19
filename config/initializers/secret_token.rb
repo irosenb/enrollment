e40450b2027f7bef13d9317f27e7070dd71aec06
Isaac Rosenberg <irosenb7@gmail.com>
2013-08-19 14:37:56 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Enrollment::Application.config.secret_token = '36d53fc4737c99357c5013befe6fef2bf694bbe75d57cd6fc4482fa30f2aa3b26464c67c0037bb2a20c9ebb5d996ce3ad958dc70bab99f74c140964e75d81814'
